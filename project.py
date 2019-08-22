b52ba33a2a2aeb5307dee5ea46e9c3eb476a229a
Yang Song <sy960320@gmail.com>
2019-08-22 17:00:40 -0300

    This commit contain the python code of projcet one

--- project.py
+++ project.py
@@ -1,34 +1,27 @@
+#!/usr/bin/env python3
 import psycopg2
 
 DBNAME = 'news'
-#1. What are the most popular three articles of all time?
+
+
+# 1. What are the most popular three articles of all time?
 def log_view(content):
-    conn = psycopg2.connect(database = DBNAME)
+    conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
-    cursor.execute("create view log_view asselect path, count(*) access_numberfrom loggroup by pathorder by path")
+    cursor.execute("create view log_view as select path, count(*) as access_number from log group by path order by path;")
     conn.commit()
     conn.close()
-    
+
 
 def articles_view(content):
-    conn = psycopg2.connect(database = DBNAME)
+    conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
     cursor.execute("create view articles_view as select articles.title, log_view.access_number from articles, log_view where log_view.path = CONCAT('/article/', articles.slug) order by access_number desc limit 3")
     conn.commit()
     conn.close()
 
-def Get_post1():
-    conn = psycopg2.connect(database = DBNAME)
-    cursor = conn.cursor()
-    cursor.execute("select * from articles_view")
-    result = cursor.fetchall()
-    print ("What are the most popular three articles of all time?:")
-    print (result)
-    print('\n')
-    conn.close()
-    #return result
 
-#2. Who are the most popular article authors of all time?
+# 2. Who are the most popular article authors of all time?
 def articles_view2(content):
     conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
@@ -36,56 +29,74 @@ def articles_view2(content):
     conn.commit()
     conn.close()
 
+
 def authors_view(content):
-    conn = psycopg2.connect(database = DBNAME)
+    conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
     cursor.execute("create view authors_view as select authors.name, sum(articles_view2.access_number) as view from authors, articles_view2 where authors.id = articles_view2.author group by authors.name order by view desc")
     conn.commit()
     conn.close()
 
-def Get_post2():
-    conn = psycopg2.connect(database = DBNAME)
-    cursor = conn.cursor()
-    cursor.execute("select * from authors_view")
-    result = cursor.fetchall()
-    print("Who are the most popular article authors of all time?")
-    print(result)
-    print('\n')
-    conn.close()
 
-#3. Find the error rate of '404 NOT FOUND'
+# 3. Find the error rate of '404 NOT FOUND'
 def error1(content):
-    conn = psycopg2.connect(database = DBNAME)
+    conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
     cursor.execute("create view error1 as select date(time), count(*) as error_number from log where status = '404 NOT FOUND' group by date(time) order by date(time)")
     conn.commit()
     conn.close()
 
-def error2(content):
-    conn = psycopg2.connect(database = DBNAME)
+
+def error2(content): 
+    conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
-    cursor.execute("create view error2 as select date(time), count(*) as total from log where status = '200 OK' group by date(time) order by date(time)")
+    cursor.execute("create view error2 as select date(time), count(*) as total from log group by date(time) order by date(time)")
     conn.commit()
     conn.close()
 
-def error_rate(content):
-    conn = psycopg2.connect(database = DBNAME)
+
+def error_rate(content): 
+    conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
-    cursor.execute("create view error_rate as select error1.date, (100.0*error1.error_number/error2.total) as view_number from error1, error2 where error1.date = error2.date order by error1.date")
+    cursor.execute("create view e_rate as select error1.date, (100.0*error1.error_number/error2.total) as rate from error1, error2 where error1.date = error2.date order by error1.date")
     conn.commit()
     conn.close()
 
-def Get_post3():
-    conn = psycopg2.connect(database = DBNAME)
+
+def Get_post():
+    File = open("output.txt", "w")
+    conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
-    cursor.execute("select * from error_rate")
+    File.write("What are the most popular three articles of all time?:\n")
+    cursor.execute("select * from articles_view")
     result = cursor.fetchall()
-    print("Find the error rate of '404 NOT FOUND':")
-    print(result)
+    output = ""
+    for data in result:
+        output += str(data)
+        output += "\n"
+    File.write(output)
+    File.write("\n")
+    File.write("Who are the most popular article authors of all time?\n")
+    cursor.execute("select * from authors_view\n")
+    result = cursor.fetchall()
+    output = ""
+    for data2 in result:
+        output += str(data2)
+        output += "\n"
+    File.write(output)
+    File.write("\n")
+    File.write("Find the error rate of '404 NOT FOUND':\n")
+    cursor.execute("select date, rate from e_rate where rate = (select max(rate) from e_rate)")
+    result = cursor.fetchall()
+    output = ""
+    for data3 in result:
+        output += str(data3)
+        output += "\n"
+    File.write(output)
+    File.close()
+    File = open("output.txt", "r")
+    print(File.read())
     conn.close()
-Get_post1()
-Get_post2()
-Get_post3()
 
 
-
+Get_post()
